249b971e74876a0d6ebaac7a9e15a10a715d7327
Amaia <61711537+amaiacss@users.noreply.github.com>
2021-10-27 12:52:32 +0200

    Update AmortizacionPrestamo.java
    Encabezado tarea eliminado

--- AmortizacionPrestamo.java
+++ AmortizacionPrestamo.java
@@ -1,13 +1,7 @@
-//	Nombre: Amaia Casas Echevarria.
-//	Fecha: 05/11/2021
-//	MÛdulo: ProgramaciÛn.
-//	UD2
-//	PROG02 Tarea EvaluaciÛn	01.Realiza un programa en Java. 
-//	DescripciÛn: Realizar un programa que proporcionar· diferentes c·lculos	para depÛsitos a plazo fijo,utilizando variables,bucles	FOR
-//	  y mÈtodos con paso de par·metros y sentencia return, con el uso de las clases Math y Scanner.
-//	Enlace YouTube a video autoevaluaciÛn: https://youtu.be/TnmQsCMlAtM
+//	Descripci√≥n: Realizar un programa que proporcionar√° diferentes c√°lculos	para dep√≥sitos a plazo fijo,utilizando variables,bucles	FOR
+//	  y m√©todos con paso de par√°metros y sentencia return, con el uso de las clases Math y Scanner.
 
-// Importar librerÌa para el uso de Scanner
+// Importar librer√≠a para el uso de Scanner
 import java.util.*;
 
 public class AmortizacionPrestamo {
@@ -18,19 +12,19 @@ public class AmortizacionPrestamo {
 	static final double INTERES_MIN = 2;
 	static final double INTERES_MAX = 5;
 
-   // MÈtodo principal, se crea el objeto Scanner, imprime una introducciÛn y llama a otros mÈtodos para calcular los datos que se piden.
+   // M√©todo principal, se crea el objeto Scanner, imprime una introducci√≥n y llama a otros m√©todos para calcular los datos que se piden.
 	public static void main(String[] args) {
    
 		 // Crear el objeto teclado para la lectura de los datos por teclado
 	    Scanner teclado = new Scanner(System.in);
 	    
-	    // IntroducciÛn
-	    System.out.println("Este es un programa para calcular las cuotas de un prÈstamo");
-	    System.out.println("Pedir· el capital del prÈstamo (euros), el interÈs anual a pagar (%) y su duraciÛn (aÒos)");
-	    System.out.println("Calcular· para cada aÒo, el capital pendiente y la cuota a pagar, intereses y amortizaciÛn");
+	    // Introducci√≥n
+	    System.out.println("Este es un programa para calcular las cuotas de un pr√©stamo");
+	    System.out.println("Pedir√° el capital del pr√©stamo (euros), el inter√©s anual a pagar (%) y su duraci√≥n (a√±os)");
+	    System.out.println("Calcular√° para cada a√±o, el capital pendiente y la cuota a pagar, intereses y amortizaci√≥n");
 	    System.out.println("Empezamos ya\n");
 	    
-	    System.out.println("Introduce la cantidad solicitada para el prÈstamo:");      
+	    System.out.println("Introduce la cantidad solicitada para el pr√©stamo:");      
 	    double prestamo = teclado.nextDouble();
 	    
 	    calculoCuotas(prestamo);
@@ -40,19 +34,19 @@ public class AmortizacionPrestamo {
 	}
 	   
 /***********************
-**M…TODO calculoCuotas**
+**M√âTODO calculoCuotas**
 ************************/
 	   
-   // Metodo que recibe un parametro double con la cantidad del prÈstamo y calcula las cuotas a pagar.
-   // Plazos entre 5 y 10 aÒos y los intereses desde 2% a 5% increment·ndose un 0,5% cada vez.
+   // Metodo que recibe un parametro double con la cantidad del pr√©stamo y calcula las cuotas a pagar.
+   // Plazos entre 5 y 10 a√±os y los intereses desde 2% a 5% increment√°ndose un 0,5% cada vez.
 	public static void calculoCuotas(double prestamo) {
 		System.out.println("Estas son las cuotas a pagar para diferentes intereses y plazos");
 		
-		// imprime los aÒos del 5 al 10
+		// imprime los a√±os del 5 al 10
 		for(int a = PLAZO_MIN; a <= PLAZO_MAX; a++) {
-			System.out.print(a + " AÒos\t");
+			System.out.print(a + " A√±os\t");
 			
-			// Realiza el calculo de las cuotas utilizando el mÈtodo cuotas() y redondea a dos digitos, en cada vuelta suma 0.5.
+			// Realiza el calculo de las cuotas utilizando el m√©todo cuotas() y redondea a dos digitos, en cada vuelta suma 0.5.
 			for(double i = INTERES_MIN; i <= INTERES_MAX; i += 0.5) {
 				System.out.print(redondea(cuotas(i, a, prestamo)) + "(" + i + "%)\t");
 			}
@@ -61,9 +55,9 @@ public class AmortizacionPrestamo {
 	}
 
 /****************
-**M…TODO cuotas**
+**M√âTODO cuotas**
 ****************/ 
-	// MÈtodo que recibe como par·metros el interes, los aÒos y la cantidad del prestamo para realizar el calculo de las cuotas.
+	// M√©todo que recibe como par√°metros el interes, los a√±os y la cantidad del prestamo para realizar el calculo de las cuotas.
 	// Devuelve un valor double.
 	public static double cuotas(double i, int a, double prestamo) {
 		//Formula prestamo * i / 1 -(1 + i) elevado a -duracion.
@@ -75,9 +69,9 @@ public class AmortizacionPrestamo {
 	}
 	
 /******************
-**M…TODO redondea**
+**M√âTODO redondea**
 *******************/  
-	// MÈtodo que recibe un parametro tipo double y lo redondea a dos dÌgitos.
+	// M√©todo que recibe un parametro tipo double y lo redondea a dos d√≠gitos.
 	// Devuelve un valor double
 	public static double redondea(double operacion) {
 		double resultado = (double)Math.round(operacion*100)/100;
@@ -85,38 +79,38 @@ public class AmortizacionPrestamo {
 	}
 	
 /*************************
-**M…TODO calculoPrestamo**
+**M√âTODO calculoPrestamo**
 **************************/  
-	 /* El mÈtodo pedir· el interes anual y el n˙mero de aÒos y calcular· el capital pendiente, la cuota anual,los intereses a pagar y el capital amortizado para cada aÒo del prestamo*/
+	 /* El m√©todo pedir√° el interes anual y el n√∫mero de a√±os y calcular√° el capital pendiente, la cuota anual,los intereses a pagar y el capital amortizado para cada a√±o del prestamo*/
 	// Recibe los parametros teclado y la cantidad del prestamo
 	public static void calculoPrestamo(Scanner teclado, double prestamo) {
 		
 		// Solicita al usuario el interes anual del prestamo
-		System.out.println("Introduce el interÈs anual que se aplicar· al prÈstamo en %:");
+		System.out.println("Introduce el inter√©s anual que se aplicar√° al pr√©stamo en %:");
 		double i_anual = teclado.nextDouble();
 		
-		// Solicita al usuario el n˙mero de aÒos.
-		System.out.println("Introduce el n˙mero de aÒos que va a durar el prÈstamo: ");
+		// Solicita al usuario el n√∫mero de a√±os.
+		System.out.println("Introduce el n√∫mero de a√±os que va a durar el pr√©stamo: ");
 		int plazo = teclado.nextInt();
 		
-		//Con esta variable actualizarÈ el capital pendiente en cada aÒo.
+		//Con esta variable actualizar√© el capital pendiente en cada a√±o.
 		double amortizacion = 0;
-		// Calculo de la cuota anual, para cada aÒo ser· la misma.
+		// Calculo de la cuota anual, para cada a√±o ser√° la misma.
 		double cuota_anual = cuotas(i_anual, plazo, prestamo);
 		
 		for(int a = 1; a <= plazo; a++) {
 			// actualizo el capital pendiente
 	         prestamo = prestamo - amortizacion;
-	        // c·lculo de los intereses
+	        // c√°lculo de los intereses
 	         double intereses = prestamo * (i_anual/100);
-	        // c·lculo de la amortizaciÛn del prestamo
+	        // c√°lculo de la amortizaci√≥n del prestamo
 	         amortizacion = cuota_anual - intereses;
          
-			System.out.println("AÒo: " + a + "\n"
+			System.out.println("A√±o: " + a + "\n"
 					+ "\tCapital Pendiente: " + redondea(prestamo) + "\n"
 					+ "\tCuota Anual: "+ redondea(cuota_anual)+ "\n"
 					+ "\tIntereses a pagar: " + redondea(intereses) + "\n"
-					+ "\tAmortizaciÛn: " + redondea(amortizacion));			
+					+ "\tAmortizaci√≥n: " + redondea(amortizacion));			
 		}
 	}
 }
